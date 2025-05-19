e75d664792032c9dea4c4c20b74b4c951bbe69ce
OlgaPio <lm4109080@gmail.com>
2025-05-19 01:56:44 -0500

    creacion de archivos py

---
+++ sintaxis_basica.py
@@ -0,0 +1,19 @@
+# Imprimir un mensaje en la consola
+print("¡Hola, bienvenido a Python!")
+
+# Declaración de variables de diferentes tipos
+num_int = 10  # Entero
+num_float = 3.14  # Decimal
+cadena = "Aprendiendo Python"  # Cadena de texto
+
+# Operaciones matemáticas simples
+suma = num_int + num_float
+multiplicacion = num_int * num_float
+
+# Concatenación de cadenas
+mensaje = cadena + " es divertido!"
+print(mensaje)
+
+# Uso de input para recibir datos del usuario
+nombre = input("¿Cuál es tu nombre? ")
+print("Hola, " + nombre + "!")
